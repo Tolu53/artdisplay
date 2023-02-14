6625f67ce00aba3b5ae156e84b1454ee19ecbee9
Tolu53 <115922354+Tolu53@users.noreply.github.com>
2023-02-14 13:48:40 -0500

    african placeholder image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Marquee from 'react-fast-marquee'
+import {GiAfrica} from 'react-icons/gi'
+import africa from '../assets/Africa.webp'
 
 const Banner = () => {
   return (
     <div>
       <div>
-        <div className='flex flex-col justify-center items-left w-full h-screen absolute top-0 p-10'>
+        <div className='flex flex-col justify-center items-left w-full h-screen absolute top-0 p-10 -z-10 '>
 <h1 className='text-[10vw]'> African </h1>
 <Marquee gradient={false} speed={200} pauseOnHover>
 <div className='text-[10vw] flex justify-evenly w-full duration-300 '>
 <a href="#"><span className='text-green-700'>Film</span> </a>
 <a href="#"><span className='text-white'>Photo</span> </a>
 <a href="#"><span className='text-green-700'>Art</span> </a>
+<img src={africa} alt="" />
 
 </div>
 </Marquee>
