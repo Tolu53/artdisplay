9ee8742024c5f5cf378fe62ceeb945326841968b
Tolu53 <115922354+Tolu53@users.noreply.github.com>
2023-02-15 01:54:16 -0500

    resposiveness improve

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
     const links = [{
         id:1,
         src:img1,
-        title:"About Us",
+        title:"About",
     },{
         id:2,
         src:img2,
@@ -36,13 +36,13 @@ const Nav = () => {
             <button onClick={() => toggleOpen(!isOpen)} className='px-4 text-white  bg-red-600 absolute bottom-28 md:bottom-80 lg:bottom-12  hover:scale-110 active:scale-90 z-50 active:rounded-[100%] duration-1000'>
             { isOpen ? < FaTimes size={30} className='duration-500' />: "See More"}
             </button>
-{isOpen  && (<div className='bg-black w-full h-[30vw] p-5 absolute bottom-0 justify-center items-center duration-500 flex'>
+{isOpen  && (<div className='bg-black w-full h-[50vw] md:h-[30vw] p-5 absolute bottom-0 justify-center items-center duration-500 flex'>
 <div className='flex justify-center items-center text-white  duration-1000'>
             {links.map(({ id, title, src}) =>(
-          <div key={id} className=' px-4 cursor-pointer first-letter:capitalize font-medium
+          <div key={id} className=' mx-2 cursor-pointer first-letter:capitalize font-medium
            hover:scale-105 duration-1000 list-none  '>
-            <a >{title}</a>
-            <img src={src} alt={title} className=' h-[15vw] object-cover aspect-video ' />
+            <a className='text-md w-full'>{title}</a>
+            <img src={src} alt={title} className=' w-[20vw] object-cover aspect-video ' />
             </div>
         ))
         }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,7 +4,7 @@ import pic1 from '../assets/img1.jpg'
 const Page = () => {
   return (
     <div>
-        <div className='w-full h-screen absolute top-0  -z-10'>
+        <div className='w-full h-screen absolute top-0 bg-black/10 -z-10'>
             <div className=' h-full flex items-center justify-center '>
                 <div className='justify-center flex items-center  '>
                     <img src={pic1} alt="" className='w-1/5 min-w-[300px]'/>
