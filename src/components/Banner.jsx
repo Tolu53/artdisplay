ecc44e528eaf8dd898009eea51c34c3a1863ce76
Tolu53 <115922354+Tolu53@users.noreply.github.com>
2023-02-15 01:53:46 -0500

    responsiveness improved

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,14 +10,13 @@ const Banner = () => {
         <div className='flex flex-col justify-center items-left w-full h-screen absolute top-0  -z-10 '>
 <h1 className=' md:text-[10vw] text-5xl px-10 '> African </h1>
 <Marquee gradient={false} speed={200} pauseOnHovr>
-<div className=' md:text-[10vw] text-5xl flex justify-evenly w-full duration-300 '>
+<div className=' md:text-[10vw] text-5xl flex justify-evenly w-[250vw] duration-300 '>
 <a href="#"><span className='text-green-700'>Film</span> </a>
 <a href="#"><span className='text-white'>Photo</span> </a>
 <a href="#"><span className='text-green-700'>Art</span> </a>
-<img src={africa} alt="" />
+<img src={africa} alt="" className='w-20 md:w-full' />
 </div>
 </Marquee>
-
 <h1 className=' md:text-[10vw] text-5xl px-10 '> Display</h1>
         </div>
       </div>
