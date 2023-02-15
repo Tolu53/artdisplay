397221a3c148309b046ec6cd0f92e6e4b13c8833
Tolu53 <115922354+Tolu53@users.noreply.github.com>
2023-02-15 00:40:53 -0500

    Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,19 +7,18 @@ const Banner = () => {
   return (
     <div>
       <div>
-        <div className='flex flex-col justify-center items-left w-full h-screen absolute top-0 p-10 -z-10 '>
-<h1 className='text-[10vw]'> African </h1>
-<Marquee gradient={false} speed={200} pauseOnHover>
-<div className='text-[10vw] flex justify-evenly w-full duration-300 '>
+        <div className='flex flex-col justify-center items-left w-full h-screen absolute top-0  -z-10 '>
+<h1 className=' md:text-[10vw] text-5xl px-10 '> African </h1>
+<Marquee gradient={false} speed={200} pauseOnHovr>
+<div className=' md:text-[10vw] text-5xl flex justify-evenly w-full duration-300 '>
 <a href="#"><span className='text-green-700'>Film</span> </a>
 <a href="#"><span className='text-white'>Photo</span> </a>
 <a href="#"><span className='text-green-700'>Art</span> </a>
 <img src={africa} alt="" />
-
 </div>
 </Marquee>
 
-<h1 className='text-[10vw]'> Display</h1>
+<h1 className=' md:text-[10vw] text-5xl px-10 '> Display</h1>
         </div>
       </div>
     </div>
